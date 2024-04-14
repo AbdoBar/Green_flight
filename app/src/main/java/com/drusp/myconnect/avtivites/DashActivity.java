164aabe2e6d9bc4c1f07732f8f343c92bb674f6a
yahia benotmane <benotmane.yahia@univ-bechar.dz>
2024-04-14 17:52:49 +0100

    Commit v2

--- app/src/main/java/com/drusp/myconnect/avtivites/DashActivity.java
+++ app/src/main/java/com/drusp/myconnect/avtivites/DashActivity.java
@@ -1,21 +1,77 @@
 package com.drusp.myconnect.avtivites;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.drusp.myconnect.R;
-
 public class DashActivity extends AppCompatActivity {
-    ImageView IMG_menu,IMG_chat;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash);
 
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageView imgNature = findViewById(R.id.img_nature);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})ImageView imgVr = findViewById(R.id.img_vr);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})ImageView imgGmae = findViewById(R.id.img_game);
+        ImageView imgMenu = findViewById(R.id.img_menu);
+        ImageView imgChat = findViewById(R.id.img_chat);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})ImageView imgTravel = findViewById(R.id.img_travel);
 
-       // IMG_menu=(ImageView) findViewById(R.id.img_menu);
-        //IMG_chat=findViewById(R.id.img_chat);
+        imgMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Add your action for generalButton2 here
+                // For example:
+                 Intent intent = new Intent(DashActivity.this, Menu_Activity.class);
+                 startActivity(intent);
+            }
+        });
+
+        imgChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Add your action
+                Intent intent = new Intent(DashActivity.this, ChatActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        imgTravel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Add your action for generalButton4 here
+            }
+        });
+
+        imgGmae.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Add your action for imgMenu here
+            }
+        });
+
+        imgNature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Add your action for imgChat here
+                Intent intent = new Intent(DashActivity.this, Green_Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        imgVr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Add your action for generalButton7 here
+                Intent intent = new Intent(DashActivity.this, Vr_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
